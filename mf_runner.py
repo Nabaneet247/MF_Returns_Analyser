af7a8791c6adbd54cb55618cfce1dd411225b962
Nabaneet247 <nabaneetroy88@gmail.com>
2021-07-02 20:39:29 +0530

    Changed Scheme List Updation Logic

--- mf_runner.py
+++ mf_runner.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from amfi_service import get_all_fund_house_and_scheme_names
 from mf_data_cleaner import clean_all_nav_data
-from mf_data_files_updater import update_schemes_in_all_data_files
+from mf_data_files_updater import update_schemes_in_data_file
 from mf_data_generator import update_org_nav_files_for_a_year
 from mf_data_plotter import plot_analysed_data_file_for_a_date
 from mf_returns_analyser import analyse_returns_for_intervals
@@ -23,9 +23,12 @@ def run():
         max_tries_per_house += 5
         new_schemes_list = get_all_fund_house_and_scheme_names(refetch_schemes_list_from_amfi=True)
 
-    update_schemes_in_all_data_files()
+    if len(new_schemes_list.index) > len(existing_schemes_list.index):
+        update_schemes_in_data_file('org nav')
+    # todo handle start of new year scenario
     current_year = str(datetime.now().year)
     update_org_nav_files_for_a_year(current_year)
+    # todo check cleanliness of latest data first
     clean_all_nav_data()
     intervals = ['30D', '3M', '6M', '1Y', '2Y', '5Y']
     update_returns_for_intervals(intervals=intervals, update_latest_returns_only=True)
